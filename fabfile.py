71e02e52a59e917789bb85647638d211ad04a2dd
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2010-11-27 03:04:14 -0200

    changed glob import to remove glob from the commands list

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,7 @@
 """
 
 from fabric.api import env, local, put, run
-from glob import glob
+import glob
 
 env.user = 'rafaelmartins'
 env.hosts = ['dev.gentoo.org']
@@ -21,10 +21,10 @@ def clean():
 def build():
     clean()
     local('mkdir -p _build', capture=False)
-    for f in [i[len('config/'):-len('.ini')] for i in glob('config/*.ini')]:
+    for f in [i[len('config/'):-len('.ini')] for i in glob.glob('config/*.ini')]:
         local('python checkbump.py config/%s.ini > _build/%s.html' % (f, f), capture=False)
 
 def upload():
     run('mkdir -p public_html/checkbump')
-    for html_file in glob('_build/*.html'):
+    for html_file in glob.glob('_build/*.html'):
         put(html_file, 'public_html/checkbump')
